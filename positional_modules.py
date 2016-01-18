6b10e99ef5e57e891f7cb824bb7f5d9445bc9a08
Tipplynne <larvalanobium@gmail.com>
2016-01-18 15:29:36 +0900

    Working on unpacking to DF problem

--- positional_modules.py
+++ positional_modules.py
@@ -8,7 +8,7 @@ def str_slicer(row):
 	position = 0
 	native = ''
 	mut_type = ''
-	print(s)
+	
 	#this function chops up the string with Mutant info, and retrieves mutational scope, position and native base
 	if len(s) == 4:
 		position = int(s[1:3])
@@ -20,20 +20,21 @@ def str_slicer(row):
 		mut_type = s[0] + s[2]
 	else:
 		print("\n\nError reading mutant positional values")
-	return position, native, mut_type #tried and failed as dict, tuple, list
+	return position, native, mut_type #BUG1p1 tried and failed as tuple, list, need to try tostring() then split method?
 
 def find_pos(dfd):
 	 
-	#this portion of the modules focuses on the DPmutant DF
+	#this portion of the module focuses on the DPmutant DF
 	global position
 	global native
 	global mut_type
 	#indexing for mut1 is dfd[5] and for mut2 is dfd[6], dfd[7] contains the SPmutants FC product value
 	#print(dfd.head)
 
-### serious issues with unpacking to pandas df - laaaame
-	stuff = ([str_slicer(row) for row in dfd.itertuples(False)])
-	#dfd["pos1"], dfd["native1"], dfd["mut_type1"])
+	###BUG1p2 serious issues with unpacking to pandas df - laaaame
+	dfd["packed1"] = ([str_slicer(row) for row in dfd.itertuples(False)])
+	
+	#dfd["pos1"], dfd["native1"], dfd["mut_type1"] = ([str_slicer(row) for row in dfd.itertuples(False)])
 	#(dfd["pos2"], dfd["native2"], dfd["mut_type2"]) = [str_slicer(dfd["mut2"])(row) for row in dfd.itertuples(False)]
 	#print(dfd.head)
 	return dfd
