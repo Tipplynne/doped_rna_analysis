89a5c1025ac028e5d14c04dfac022819443824ac
Tipplynne <larvalanobium@gmail.com>
2016-01-20 07:39:39 +0900

    working on the unpacking to pandas thing

--- positional_modules.py
+++ positional_modules.py
@@ -11,28 +11,32 @@ def str_slicer(row):
 	
 	#this function chops up the string with Mutant info, and retrieves mutational scope, position and native base
 	if len(s) == 4:
-		position = int(s[1:3])
+		position = s[1:3]
 		native = s[0]
 		mut_type = s[0] + s[3]
 	elif len(s) == 3:
-		position = int(s[1:2])
+		position = s[1:2]
 		native = s[0]
 		mut_type = s[0] + s[2]
 	else:
 		print("\n\nError reading mutant positional values")
-	return position, native, mut_type #BUG1p1 tried and failed as tuple, list, need to try tostring() then split method?
+	
+	return position, native, mut_type #BUG1p1 tried and failed as tuple, list, need to try tostring() then split method? Thats fucking retarded
 
 def find_pos(dfd):
 	 
 	#this portion of the module focuses on the DPmutant DF
-	global position
-	global native
-	global mut_type
+	
 	#indexing for mut1 is dfd[5] and for mut2 is dfd[6], dfd[7] contains the SPmutants FC product value
 	#print(dfd.head)
 
 	###BUG1p2 serious issues with unpacking to pandas df - laaaame
+	#Soln from 
 	dfd["packed1"] = ([str_slicer(row) for row in dfd.itertuples(False)])
+	unpack = ['position1', 'native1', 'mut_type1']
+	dfd[['mut1']].join(dfd.packed1.apply(lambda loc: Series(loc, index=unpack))) #error: name 'Series' is not defined
+
+	#df = df.drop('location',axis=1)
 	
 	#dfd["pos1"], dfd["native1"], dfd["mut_type1"] = ([str_slicer(row) for row in dfd.itertuples(False)])
 	#(dfd["pos2"], dfd["native2"], dfd["mut_type2"]) = [str_slicer(dfd["mut2"])(row) for row in dfd.itertuples(False)]
