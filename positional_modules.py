48f846e64e9e301126820bb21d30031d35dc51a6
Tipplynne <larvalanobium@gmail.com>
2016-01-20 10:55:25 +0900

    positional module problem solved yay!

--- positional_modules.py
+++ positional_modules.py
@@ -4,12 +4,12 @@ import numpy as np
 import pandas as pd
 
 def str_slicer(row):
-	s = row[5]
+	s = row
 	position = 0
 	native = ''
 	mut_type = ''
 	
-	#this function chops up the string with Mutant info, and retrieves mutational scope, position and native base
+	#this function chops up the string with Mutant info, and retrieves mutational scope, position and native base as a packed column. string may be composed of 3 or 4 chars eg. A9C or A25C, where A is the native base, A-> C is scope and 9 and 25, are the respective mutation positions 
 	if len(s) == 4:
 		position = s[1:3]
 		native = s[0]
@@ -21,24 +21,24 @@ def str_slicer(row):
 	else:
 		print("\n\nError reading mutant positional values")
 	
-	return position, native, mut_type #BUG1p1 tried and failed as tuple, list, need to try tostring() then split method? Thats fucking retarded
+	return position, native, mut_type 
 
 def find_pos(dfd):
 	 
-	#this portion of the module focuses on the DPmutant DF
+	#this fxn focuses on the DPmutant DF, uses the string slicer to extract info
 	
 	#indexing for mut1 is dfd[5] and for mut2 is dfd[6], dfd[7] contains the SPmutants FC product value
-	#print(dfd.head)
-
-	###BUG1p2 serious issues with unpacking to pandas df - laaaame
-	#Soln from 
-	dfd["packed1"] = ([str_slicer(row) for row in dfd.itertuples(False)])
-	unpack = ['position1', 'native1', 'mut_type1']
-	dfd[['mut1']].join(dfd.packed1.apply(lambda loc: Series(loc, index=unpack))) #error: name 'Series' is not defined
-
-	#df = df.drop('location',axis=1)
 	
-	#dfd["pos1"], dfd["native1"], dfd["mut_type1"] = ([str_slicer(row) for row in dfd.itertuples(False)])
-	#(dfd["pos2"], dfd["native2"], dfd["mut_type2"]) = [str_slicer(dfd["mut2"])(row) for row in dfd.itertuples(False)]
-	#print(dfd.head)
+	dfd["packed1"] = ([str_slicer(row[5]) for row in dfd.itertuples(False)])
+	dfd["packed2"] = ([str_slicer(row[6]) for row in dfd.itertuples(False)])
+	unpack = ('position1', 'native1', 'mut_type1')
+	unpack2 = ('position2', 'native2', 'mut_type2')
+	#unpack etc is new column names, create new dfs for each mutation
+	df1 = dfd.packed1.apply(lambda loc: pd.Series(loc, index = unpack))
+	df2 = dfd.packed2.apply(lambda loc: pd.Series(loc, index = unpack))
+	#appends new df to dfd, and drops temporary packed columns
+	dfd = pd.concat([dfd, df1, df2], axis=1, join_axes=[dfd.index])
+	dfd = dfd.drop('packed1',axis=1)
+	dfd = dfd.drop('packed2',axis=1)
+	
 	return dfd
